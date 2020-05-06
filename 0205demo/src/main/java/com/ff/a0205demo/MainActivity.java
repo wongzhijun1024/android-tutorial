1d4eba6288e5f7fd38dea5ee0b2fe0361b700821
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2020-05-06 09:35:32 +0800

    更新代码

--- 0205demo/src/main/java/com/ff/a0205demo/MainActivity.java
+++ 0205demo/src/main/java/com/ff/a0205demo/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //获得组件
         getWidget();
     }
 

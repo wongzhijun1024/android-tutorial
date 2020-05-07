71e5440708bbd0bffab0a7ea129002385653296a
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2020-05-07 08:24:32 +0800

    update the function of select the  menu

--- 0205demo/src/main/java/com/ff/a0205demo/MainActivity.java
+++ 0205demo/src/main/java/com/ff/a0205demo/MainActivity.java
@@ -10,6 +10,17 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //单词选项
+    final int STATE_BOOK=1;
+    //训练
+    final int STATE_TRAINING=STATE_BOOK+1;
+    //查找
+    final int STATE_SEARCH=STATE_TRAINING+1;
+    //商城
+    final int STATE_SHOP=STATE_SEARCH+1;
+    //我的
+    final int STATE_MINE=STATE_SHOP+1;
+
     RelativeLayout bookLayout;
     TextView bookIcon;
     TextView bookFont;
@@ -17,6 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     RelativeLayout trainingLayout;
     TextView trainingIcon;
     TextView trainingFont;
+
+    //创建一个缓冲的容器
+    int index=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +45,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void getWidget() {
         getBookWidget();
-        gettrainingWidget();
+        getTrainingWidget();
     }
 
-    private void gettrainingWidget() {
+    /**
+     * 获得训练组件
+     */
+    private void getTrainingWidget() {
+        //获得容器
         trainingLayout = this.findViewById(R.id.trainingLayout);
+        //获得图片
         trainingIcon=this.findViewById(R.id.trainingIcon);
+        //获得字体
         trainingFont=this.findViewById(R.id.trainingFont);
-
+        //设置监听
         trainingLayout.setOnClickListener(this);
     }
 
@@ -46,15 +66,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 获得单词组件
      */
     private void getBookWidget() {
+        //获得显示容器
         bookLayout = this.findViewById(R.id.bookLayout);
+        //获得图片
         bookIcon=this.findViewById(R.id.bookIcon);
+        //获得字体
         bookFont=this.findViewById(R.id.bookFont);
+        //设置监听
         bookLayout.setOnClickListener(this);
+        index=STATE_BOOK;
 
-    }
+}
 
     @Override
     public void onClick(View view) {
+        reset();
         int id  = view.getId();
         switch(id){
             case R.id.bookLayout:
@@ -66,15 +92,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    @SuppressLint("ResourceAsColor")
+    private void reset(){
+     switch(index){
+         case STATE_BOOK:
+             bookIcon.setBackgroundResource(R.drawable.book2);
+             bookFont.setTextColor(R.color.fontNoSelect);
+             break;
+         case STATE_TRAINING:
+             trainingIcon.setBackgroundResource(R.drawable.training2);
+             trainingFont.setTextColor(R.color.fontNoSelect);
+             break;
+     }
+    }
     @SuppressLint("ResourceAsColor")
     private void trainingAction() {
         trainingIcon.setBackgroundResource(R.drawable.training);
         trainingFont.setTextColor(R.color.fontSelect);
+        index=STATE_TRAINING;
     }
 
     @SuppressLint("ResourceAsColor")
     private void bookAction() {
         bookIcon.setBackgroundResource(R.drawable.book1);
         bookFont.setTextColor(R.color.fontSelect);
+        index=STATE_BOOK;
     }
 }

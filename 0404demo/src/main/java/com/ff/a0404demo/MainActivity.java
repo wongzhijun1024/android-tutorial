053ac3f10573828686b7e6134000f20c25948889
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2020-05-21 08:24:06 +0800

    1，添加标题页面 2，添加中间显示容器 3，设置每个组件的高度

--- 0404demo/src/main/java/com/ff/a0404demo/MainActivity.java
+++ 0404demo/src/main/java/com/ff/a0404demo/MainActivity.java
@@ -3,32 +3,87 @@ package com.ff.a0404demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.ff.a0404demo.widget.MyMenu;
 import com.ff.a0404demo.widget.OnMenuClickListener;
 
 public class MainActivity extends Activity implements OnMenuClickListener {
 
+    /**
+     * 标题布局
+     */
+    RelativeLayout layoutTitle;
+    /**
+     * 容器布局
+     */
+    RelativeLayout layoutBox;
+    /**
+     * 菜单
+     */
     MyMenu myMenu;
+
+    /**
+     * 屏幕宽度
+     */
+    private  int width;
+
+    /**
+     * 屏幕高度
+     */
+    private int height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initData();
         getWidget();
     }
 
-    private void getWidget() {
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        Display defaultDisplay = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        defaultDisplay.getSize(point);
+        width = point.x;
+        height = point.y;
+    }
 
+    private void getWidget() {
+        getWidgeTitle();
+        getWidgetBox();
         getWidgetMenu();
     }
 
+
+    private void getWidgeTitle() {
+        layoutTitle = this.findViewById(R.id.layout_title);
+
+        RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) layoutTitle.getLayoutParams();
+        params.height=height/10;
+    }
+
+    private void getWidgetBox() {
+        //获得显示容器：用来装中间切换的内容
+        layoutBox =this.findViewById(R.id.layout_box);
+        //获得布局的属性
+        RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) layoutBox.getLayoutParams();
+        params.height=height/10*8;
+
+    }
     private void getWidgetMenu() {
         myMenu = this.findViewById(R.id.myMenu);
         myMenu.setOnMenuClickListener(this);
+        RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) myMenu.getLayoutParams();
+        params.height=height/10;
     }
 
     @Override
